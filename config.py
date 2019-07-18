81876bb2565f05aa54948d5f7f3d1a1112b4cd0b
Zac Shaiken <zacs@Zacs-MacBook-Pro.local>
2019-07-18 04:31:27 -0700

    added solstice

--- config.py
+++ config.py
@@ -4,4 +4,5 @@ def get_config():
 		"nHBTBhw1uasg6nGkw7ASNCrHjzG8x4Hr4PkE5qoLs83tWL2RTFJk":"$twitter.xrptipbot.com/WietseWind",
 		"nHUcNC5ni7XjVYfCMe38Rm3KQaq27jw7wJpcUYdo4miWwpNePRTw":"$twitter.xrptipbot.com/RabbitKickClub",
 		"nHB8QMKGt9VB4Vg71VszjBVQnDW3v3QudM4DwFaJfy96bj4Pv9fA":"$twitter.xrptipbot.com/bithomp",
+		"nHUT5yg3QwqwaVN3qcCJ46vSSTNXR1zfXUapp7xzB2gw6tMuJwq2":"$twitter.xrptipbot.com/Solstice_Info",
 	}
