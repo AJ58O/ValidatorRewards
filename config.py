b7d13113ea73c888edff703bd9b19b7cc8b1e1d5
Zac Shaiken <zacs@Zacs-MacBook-Pro.local>
2019-07-18 04:28:32 -0700

    added wietsewind, rabbitkickclub, and bithomp

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 def get_config():
 	return {
 		"validator_public_key":"ilp_payment_pointer",
-		# Example: "nHUFE9prPXPrHcG3SkwP1UzAQbSphqyQkQK9ATXLZsfkezhhda3p":"$twitter.xrptipbot.com/AJ58O",
+		"nHBTBhw1uasg6nGkw7ASNCrHjzG8x4Hr4PkE5qoLs83tWL2RTFJk":"$twitter.xrptipbot.com/WietseWind",
+		"nHUcNC5ni7XjVYfCMe38Rm3KQaq27jw7wJpcUYdo4miWwpNePRTw":"$twitter.xrptipbot.com/RabbitKickClub",
+		"nHB8QMKGt9VB4Vg71VszjBVQnDW3v3QudM4DwFaJfy96bj4Pv9fA":"$twitter.xrptipbot.com/bithomp",
 	}
