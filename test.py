6d18229929feaca875891ed55f71fdc96c614998
AJ58O <breakthroughconsultations@gmail.com>
2019-09-15 02:11:29 -0700

    use xrptipbot

--- test.py
+++ test.py
@@ -1,21 +1,87 @@
 from validatorScoreCard import ValidatorScoreCard
 from rippleStats import RippleStats
-from paymentHandler import ilpTrigger
 from config import get_config
-import time
+import xrptipbotPy
+import os
+import sys
+import datetime
+import json
 
-time.sleep(15) #to wait for moneyd to start
-validator_config = get_config()
-validator_pubkey_list = list(validator_config.keys())
-rs=RippleStats()
-validator_report_list = rs.get_validator_reports_from_list(validator_pubkey_list)
+try:
+	bounty = float(os.environ['BOUNTY'])
+except:
+	bounty = .05
 
-# validator_report_list = rs.list_validators()["validators"] ADD A WAY TO CRAWL MANIFESTS AND ASK NODE OPERATORS TO ADD A CUSTOM VALUE, THIS CAN BE ANOTHER OPTION INSTEAD OF THE STATIC CONFIG
+try:
+	x = xrptipbotPy.xrptipbot(os.environ['XRPTIPBOT_TOKEN'])
+except Exception as e:
+	logging.exception("ERROR")
+	print("export XRPTIPBOT_TOKEN")
+	sys.exit()
 
-vsc=ValidatorScoreCard(validator_report_list)
+def create_log(balance, amount, success, fail):
+	t = datetime.datetime.now()
+	s = ""
+	for suc in success:
+		s += suc.replace("xrptipbot://twitter/","") + "\n"
+	f = ""
+	for fai in fail:
+		f += fai.replace("xrptipbot://twitter/","")+ "\n"
+	message = """
+---------------------------
+Runtime: {0}
+Amount: {1}
+Balance: {2}
+Success:
+{3}
 
-good_validator_keys = vsc.grep_all(return_keys=True)
-good_validator_payment_pointers = [validator_config[x] for x in good_validator_keys]
-print(good_validator_payment_pointers)
+************
+Fail:
+{4}
+************
+---------------------------
+	""".format(t, amount, balance, s, f)
+	return message
 
-ilpTrigger(good_validator_payment_pointers, .5)
+def bytes_to_json(byteString):
+	return json.loads(byteString.decode('utf8').replace("'", '"'))
+
+
+def tip(amount, recipient):
+	return x.tip(amount, recipient, True).json()
+
+
+def main():
+	print("starting")
+	validator_config = get_config()
+	validator_pubkey_list = list(validator_config.keys())
+	rs=RippleStats()
+	print("getting validatior reports")
+	validator_report_list = rs.get_validator_reports_from_list(validator_pubkey_list)
+
+	# validator_report_list = rs.list_validators()["validators"] ADD A WAY TO CRAWL MANIFESTS AND ASK NODE OPERATORS TO ADD A CUSTOM VALUE, THIS CAN BE ANOTHER OPTION INSTEAD OF THE STATIC CONFIG
+	print("getting score cards")
+	vsc=ValidatorScoreCard(validator_report_list)
+	good_validator_keys = vsc.grep_all(return_keys=True)
+	print("preparing payment info")
+	good_validator_payment_pointers = [validator_config[x] for x in good_validator_keys]
+	amount = round(bounty / len(good_validator_payment_pointers), 6) - .00001
+	success = []
+	fail = []
+	print("making payments")
+	for v in good_validator_payment_pointers:
+		t = tip(amount, v)
+		if t["error"] == False:
+			success.append(v)
+			balance = t["data"]["balance"]["balance"]["XRP"]
+		else:
+			fail.append(v)
+	l = create_log(balance, amount, success, fail)
+	print(l)
+
+
+
+		
+
+
+main()
